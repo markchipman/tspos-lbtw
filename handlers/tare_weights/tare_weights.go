b6a3381b880cae65e0b89e9b60c9f2e449245d26
wormling <wormling@gmail.com>
2017-02-18 14:21:24 +0200

    rfc5988 link headers for first/last/next/prev

--- handlers/tare_weights/tare_weights.go
+++ handlers/tare_weights/tare_weights.go
@@ -1,12 +1,18 @@
 package tare_weights
 
 import (
-	"github.com/wormling/tspos-lbtw/models"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
 	"gopkg.in/gin-gonic/gin.v1"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"net/http"
-	"time"
+
+	"github.com/wormling/tspos-lbtw/models"
 )
 
 func Create(c *gin.Context) {
@@ -39,10 +45,68 @@ func Get(c *gin.Context) {
 func List(c *gin.Context) {
 	db := c.MustGet("db").(*mgo.Database)
 	tareWeights := []models.TareWeight{}
-	err := db.C(models.CollectionTareWeights).Find(nil).Sort("-updated_on").All(&tareWeights)
+	//query := models.TareWeight{}
+
+	var query map[string]string
+	var ss []string
+	var page int = 0
+	var per_page int = 10
+
+	ss = strings.Split(c.Request.URL.RawQuery, "&")
+	query = make(map[string]string)
+	for _, pair := range ss {
+		z := strings.Split(pair, "=")
+		switch z[0] {
+		case "page":
+			page, _ = strconv.Atoi(z[1])
+		case "per_page":
+			per_page, _ = strconv.Atoi(z[1])
+		default:
+			query[z[0]], _ = url.QueryUnescape(z[1])
+		}
+
+	}
+
+	err := db.C(models.CollectionTareWeights).Find(query).Skip(page * per_page).Limit(per_page).Sort("-updated_on").All(&tareWeights)
 	if err != nil {
 		c.Error(err)
 	}
+
+	var count int = 0
+	count, err = db.C(models.CollectionTareWeights).Find(query).Count()
+
+	// Build first link
+	firstQuery := c.Request.URL.Query()
+	firstQuery.Set("page", "0")
+	firstQuery.Set("per_page", string(per_page))
+	first := c.Request
+	first.URL.RawQuery = firstQuery.Encode()
+
+	// Build last link
+	lastQuery := c.Request.URL.Query()
+	lastQuery.Set("page", string(count/per_page))
+	lastQuery.Set("per_page", string(per_page))
+	last := c.Request
+	last.URL.RawQuery = lastQuery.Encode()
+
+	// Build prev link
+	prevQuery := c.Request.URL.Query()
+	prevQuery.Set("page", string(page))
+	prevQuery.Set("per_page", string(per_page))
+	prev := c.Request
+	prev.URL.RawQuery = prevQuery.Encode()
+
+	// Build next link
+	nextQuery := c.Request.URL.Query()
+	nextQuery.Set("page", string(page))
+	nextQuery.Set("per_page", string(per_page))
+	next := c.Request
+	next.URL.RawQuery = nextQuery.Encode()
+
+	// rfc5988
+	links := fmt.Sprintf("<%s>; rel=\"next\" <%s>; rel=\"prev\" <%s>; rel=\"first\" <%s>; rel=\"last\"", prev.RequestURI, next.RequestURI, first.RequestURI, last.RequestURI)
+	c.Header("Link", links)
+
 	c.JSON(http.StatusOK, gin.H{
 		"tareWeights": tareWeights,
 	})
